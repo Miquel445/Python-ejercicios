3a5549f70bd5f07fd8b60e6a4f40b1346c61c151
Miquel <144721218+Miquel445@users.noreply.github.com>
2024-04-17 19:54:16 +0200

    Add files via upload

---
+++ Ejercicos2.py
@@ -0,0 +1,12 @@
+print("H ola que tal?:")
+primo = int(input("Cuantas veces hay que esctibir Cuñao: "))
+cunao = 0
+while cunao < primo:
+   print("Hola Cuñao")
+   cunao += 1
+print("Eres un pedazo de cuñao \n pedaszo de Cuñao")  
+
+#Cuantas veces tienes que esctibir cuñao
+premoh = "CUÑAOOOO"
+for I in premoh:
+    print (I)
