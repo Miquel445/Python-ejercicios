7da5380db53319ef7782e3de273325dcfa71c670
Miquel445 <144721218+Miquel445@users.noreply.github.com>
2024-04-23 19:33:25 +0200

    Ejericios del martes

---
+++ Ejercicos2.py
@@ -0,0 +1,47 @@
+print("H ola que tal?:")
+primo = int(input("Cuantas veces hay que esctibir Cuñao: "))
+cunao = 0
+while cunao < primo:
+   print("Hola Cuñao")
+   cunao += 1
+print("Eres un pedazo de cuñao \n pedaszo de Cuñao")  
+
+#Cuantas veces tienes que esctibir cuñao
+premoh = "CUÑAOOOO"
+for I in premoh:
+    print (I)
+
+
+def generaPares(limite):
+        num=1
+
+        while num<limite:
+            yield num*2
+
+            num=num+1
+
+devuelvePares=generaPares(10)
+
+
+print(next(devuelvePares))
+
+
+print("Aquí va a ir mucho más codigo...")
+
+print(next(devuelvePares))
+
+print("Aquí va a ir mucho más codigo...")
+
+print(next(devuelvePares))
+
+
+def devuelve_ciudades(*ciudades):
+     for elemento in ciudades:
+          yield elemento
+
+
+ciudades_devueltas=devuelve_ciudades("Madrid", "Barcelona", "Valencia", "Bilvao")
+
+print(next(ciudades_devueltas))
+
+print(next(ciudades_devueltas))
